0b2cd52f039f8a0ade4426f6cf6f32bf9d7415be
DivyamSingh <64833579+DivyamSingh18@users.noreply.github.com>
2022-12-16 22:52:03 +0530

    Update 4 input function.py
     In Python, we use input() function to take input from the user. Whatever you enter as input, the input function converts it 
           into a string. If you enter an integer value still input() function convert it into a string.

--- 4 input function.py
+++ 4 input function.py
@@ -9,3 +9,4 @@ print('Nice to meet uh, ', x)
 '''    In Python, we use input() function to take input from the user. Whatever you enter as input, the input function converts it 
        into a string. If you enter an integer value still input() function convert it into a string.
 '''
+ 
