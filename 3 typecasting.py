7f1f641fb5eda028e38c3abfd532c2ddc6525646
DivyamSingh <64833579+DivyamSingh18@users.noreply.github.com>
2022-12-16 22:32:27 +0530

    Update 3 typecasting.py

--- 3 typecasting.py
+++ 3 typecasting.py
@@ -18,7 +18,7 @@ z = float("3")   # z will be 3.0
 w = float("4.2") # w will be 4.2
 
 '''
-he conversion of one data type into the other data type is known as type casting in python or type conversion in python. Python supports a wide
+the conversion of one data type into the other data type is known as type casting in python or type conversion in python. Python supports a wide
 variety of functions or methods like: int(), float(), str(), ord(), hex(), oct(), tuple(), set(), list(), dict(), etc. for the type casting in python.
 
 There are two varieties of typecasting in python namely 
