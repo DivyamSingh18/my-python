69a4c5db25014079cc7e5c2b2115dade8dd95054
DivyamSingh <64833579+DivyamSingh18@users.noreply.github.com>
2022-12-16 21:03:48 +0530

    Create 2 data types.py

---
+++ 2 data types.py
@@ -0,0 +1,27 @@
+a = complex(8, 2)
+b = True
+c = "Divyam"
+d = None
+e = 9
+f = [8, 2.3, [-4, 5], ["apple", "banana"]]
+g = (("parrot", "sparrow"), ("Lion", "Tiger"))
+h = {"name": "Sakshi", "age": 20, "canVote": True}
+
+print("a is printed as", a, "and type of a is ", type(a))
+print("b is printed as", b, "and type of a is ", type(b))
+print("c is printed as", c, "and type of a is ", type(c))
+print("d is printed as", d, "and type of a is ", type(d))
+print("e is printed as", e, "and type of a is ", type(e))
+print("f is printed as", f, "and type of a is ", type(f))
+print("g is printed as", g, "and type of a is ", type(g))
+print("h is printed as", h, "and type of a is ", type(h))   
+
+# output:
+    # a is printed as (8+2j) and type of a is  <class 'complex'>
+    # b is printed as True and type of a is  <class 'bool'>
+    # c is printed as Divyam and type of a is  <class 'str'>
+    # d is printed as None and type of a is  <class 'NoneType'>
+    # e is printed as 9 and type of a is  <class 'int'>
+    # f is printed as [8, 2.3, [-4, 5], ['apple', 'banana']] and type of a is  <class 'list'>
+    # g is printed as (('parrot', 'sparrow'), ('Lion', 'Tiger')) and type of a is  <class 'tuple'>
+    # h is printed as {'name': 'Sakshi', 'age': 20, 'canVote': True} and type of a is  <class 'dict'>
