21ccb85643cd554a9e2707c675bf0af4005c899e
DivyamSingh <64833579+DivyamSingh18@users.noreply.github.com>
2022-12-16 18:38:42 +0530

    Update 1 print statement.py

--- 1 print statement.py
+++ 1 print statement.py
@@ -1,3 +1,18 @@
+###################################### Description ############################################################################################ 
+# print(object(s), sep=separator, end=end, file=file, flush=flush)
+
+#           Parameter	Description:
+#           object(s)                 	Any object, and as many as you like. Will be converted to string before printed
+#           sep='separator'	            Optional. Specify how to separate the objects, if there is more than one. Default is ' '
+#           end='end'                  	Optional. Specify what to print at the end. Default is '\n' (line feed)
+#           file	                      Optional. An object with a write method. Default is sys.stdout
+#           flush	                      Optional. A Boolean, specifying if the output is flushed (True) or buffered (False). Default is False
+####################################### Description ############################################################################################ 
+
+
+
+
+
 print("hello world")   # prints or displays "hello world"
 print('hello world')  # does the same as above
 print(5)
