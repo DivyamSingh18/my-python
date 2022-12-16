009e590c248009f38e6c6023cb17c70aeb6ef6d2
DivyamSingh <64833579+DivyamSingh18@users.noreply.github.com>
2022-12-16 22:51:34 +0530

    Create 4 input function.py

---
+++ 4 input function.py
@@ -0,0 +1,11 @@
+x = input('whats your name? ')
+print('Nice to meet uh, ', x)
+
+# OUTPUT:
+    # whats your name? Divyam                            
+    # Nice to meet uh,  Divyam
+    
+
+'''    In Python, we use input() function to take input from the user. Whatever you enter as input, the input function converts it 
+       into a string. If you enter an integer value still input() function convert it into a string.
+'''
